309f14f80fdf5b62931da2074d2aeab1b063d7fa
i7medo <abdulhmeedgh94@gmail.com>
2020-05-12 18:30:22 +0300

    toGetNewChanges

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-
 import {Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+
 var sectionStyle = {
     width: "100%",
     height: "570px",
